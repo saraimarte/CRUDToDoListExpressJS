c983e7c70e3bf3cdbd3be3ab81879aa1d65ba8ac
Sarai Marte <smartepujols@gmail.com>
2025-01-18 22:11:40 -0500

    011825 added controllers and comments

--- server.js
+++ server.js
@@ -1,74 +1,30 @@
+//Initialize express app - creates an http server by default 
 const express = require('express');
-
 const app = express();
 
+//Middleware //This is needed so that ExpressJS can actually read the JSON in the body sent from the client like so (req.body.task)
+// By default http cannot send objects so we make it a string on the client and then we 'unstring' it using this middleware that simply turns the string into a JSON object. 
 app.use(express.json());
 
+// Importing controllers - controllers are just functions that control what happens when the client wants to go to a certain page/path.
+const {getTasks, addTask, deleteTask, updateTask} = require('./Controllers/controllers');
 
-let tasks = [
-    {id: 1, task: 'clean room'},
-    {id: 2, task: 'do homework'}
-]
-
+//Home Page
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html'); //__direname only works like this if using the commonJS mode of importing modules instead of the other way which is the ES6 modules.
 })
 
-app.get('/api/tasks/', (req, res) => {
-    return res.status(200).json(tasks);
-})
+//Get all tasks 
+app.get('/api/tasks/', getTasks);
 
-app.post('/api/tasks/', (req, res) => {
+//Add a task
+app.post('/api/tasks/', addTask );
 
-    console.log(req.body.task);
-    
-    const newTask = {
-        id: tasks.length + 1,
-        task: req.body.task,
-    }
+//Delete a task
+app.delete('/api/tasks/:id/', deleteTask);
 
-    tasks.push(newTask);
-
-    return res.status(201).json(tasks); //201 means successful and something was created
-})
-
-
-app.delete('/api/tasks/:id/', (req, res) => {
-
-    const id = parseInt(req.params.id);
-    //const id = req.body.id;
-    console.log(`ID: ${id}`);
-
-    //only leave tasks that do not have this id 
-
-    tasks =  tasks.filter((task) => task.id !== id);
-
-    
-    return res.status(200).json(tasks);
-  
-})
-
-
-
-app.put('/api/tasks/:id/', (req, res) => {
-
-    const id = parseInt(req.params.id);
-    //const id = req.body.id;
-    console.log(`ID: ${id}`);
-
-    const editedTask = req.body.task;
-
-    console.log(`editedTask: ${editedTask}`);
-
-    const taskChosen =  tasks.find((task) => task.id === id);
-
-    console.log(`taskChosen: ${taskChosen.task}`);
-
-    taskChosen.task = editedTask;
-
-    return res.status(200).json(tasks);
-  
-})
+//Update a task 
+app.put('/api/tasks/:id/', updateTask);
 
 app.listen('3000', () => {
     console.log("Listening to Port 3000...");
